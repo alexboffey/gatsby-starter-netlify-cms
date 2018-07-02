be97605b97fb8aa92becf8ca496ae6f76532c0f6
Alex Boffey <aboffey@iweb.co.uk>
2018-07-02 19:42:04 +0100

    Added Work post type

---
+++ src/cms/preview-templates/WorkPostPreview.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { WorkPostTemplate } from '../../templates/Work-post'
+
+const WorkPostPreview = ({ entry, widgetFor }) => (
+  <WorkPostTemplate
+    content={widgetFor('body')}
+    description={entry.getIn(['data', 'description'])}
+    tags={entry.getIn(['data', 'tags'])}
+    title={entry.getIn(['data', 'title'])}
+  />
+)
+
+WorkPostPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default WorkPostPreview
--- src/pages/work/2018-07-02-this-is-a-test.md
+++ src/pages/work/2018-07-02-this-is-a-test.md
@@ -1,5 +1,5 @@
 ---
-templateKey: blog-post
+templateKey: work-post
 title: This is a test
 date: '2018-07-02T19:16:11+01:00'
 description: Hey this is jsut a tesest
